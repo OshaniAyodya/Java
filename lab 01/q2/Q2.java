e20eaaa6e151c931cc9925a20500f086ccec89b4
OshaniAyodya <137174382+OshaniAyodya@users.noreply.github.com>
2023-07-30 19:44:58 +0000

    Add files via upload

---
+++ lab 01/q2/Q2.java
@@ -0,0 +1,8 @@
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Oshani");
+        System.out.println("I am a undergraduate of Management information System");
+    }
+}
---
+++ lab 01/q2/src/main/java/com/mycompany/q2/Q2.java
@@ -0,0 +1,8 @@
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Oshani");
+        System.out.println("My degree is Management information System");
+    }
+}
